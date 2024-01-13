8b274c65c0acb3890e48de2f42305fcda0cca8bf
AkshatBari02 <akshatbari.ab@gmail.com>
2024-01-13 23:30:50 +0530

    image preload

---
+++ preload.js
@@ -0,0 +1,10 @@
+function preloadImage(url) {
+    var img = new Image();
+    img.src = url;
+}
+
+var imageUrl = 'bg-amplify.jpeg';
+preloadImage(imageUrl);
+window.onload = function() {
+    document.querySelector('.container').classList.remove('hide');
+};
